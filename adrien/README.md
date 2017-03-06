ee2ae2781f5fcbad50349daccce429ccb99e1390
Clement <lamouche444@hotmail.fr>
2017-03-06 11:24:39 +0100

    correction de bug

--- adrien/README.md
+++
@@ -1,2 +0,0 @@
-# Change your way
-The best game ever 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,55 @@
 window.onload=start;
+var table = new Array(); // on cree le tableau bleu, contenant les lignes
+var larg;
+var haut;
+var niveau;
+
+
 //document.onkeydown=move;
-// on cree le tableau bleu, contenant les lignes
-var table = new Array();
+
 // on parcourt les lignes...
-for(var i=0; i<20; i++){
+/*for(var i=0; i<20; i++){
 	table[i] = new Array();
    // ... et dans chaque ligne, on parcourt les cellules
 	for(var j=0; j<20; j++){
     	table[i][j] = 0;
     }
-}
+}*/
 
-var larg;
-var haut;
-var niveau;
+
 
 function start() {
-	niveau=1;
-	niveau1();
+	//On cache le bouton "niveau suivant"
+	var bouton=document.getElementById("niveau");
+	bouton.style.visibility="hidden";
+
+	if(niveau==undefined){
+		niveau=1;
+		niveau1();
+	}
+	if(niveau==1){
+		niveau1();
+	}
+	if(niveau==2){
+		niveau2();
+	}
+	displayTemplate(table);
+
+/*	for(var i=0;i<=19;i++) {
+		for(var j=0;j<=19;j++) {
+			if(table[i][j]==1) {
+				afficherMur(i,j);
+			}
+		}
+	}*/
 	/*
 	var alea=Math.floor(Math.random()*20);
 	table[19][alea]=2;
 	document.getElementById("coffre").style.top=19*20+'px';
 	document.getElementById("coffre").style.left=20*alea+'px';
 	*/
-	document.getElementById("pic2").style.top='0px';
-	document.getElementById("pic2").style.left='0px';
-	larg=0;
-	haut=0;
+
+
 	/*
 	if(table[0][1]==1) {
 		document.getElementById("mine").innerHTML++;
@@ -38,8 +60,199 @@ function start() {
 	*/
 }
 
+function reset(template){
+	for(var i=0;i<template.length;i++){
+		for(var j=0;j<template[i].length;j++){
+	    	table[i][j] = 0;
+	    	enleverMur(i,j);
+	    }
+	}
+}
+
+// gestion des mouvements joueurs
+function move(event) {
+	var key=event.keyCode;
+
+	var smile=document.getElementById("pic2");
+	var x=smile.offsetLeft;
+	var y=smile.offsetTop;
+	table[haut][larg]=0; // On modifie la valeur de la case de départ pour pouvoir y re-passer
+
+	if(key==65) { // 65 représente la touche "a"
+		cheat();
+	}
+
+	if(key==37) { // 37 représente la touche fleche gauche
+		if(x!=0) {
+			if(table[haut][larg-1]==0) {
+				smile.style.left=x-20+'px';
+				larg--;
+				document.getElementById('largeur').innerHTML = larg;
+				document.getElementById("pas").innerHTML++;
+			} else if(table[haut][larg-1]==3) {
+				win();
+			}
+		}
+	}
+
+	if(key==38) { // 38 représente la touche fleche haut
+		if(y!=0) {
+			if(table[haut-1][larg]==0) {
+				smile.style.top=y-20+'px';
+				haut--;
+				document.getElementById('haut').innerHTML = haut;
+				document.getElementById("pas").innerHTML++;
+			} else if(table[haut-1][larg]==3) {
+				win();
+			}
+		}
+	}
+
+	if(key==39) { // 39 représente la touche fleche droite
+		if(x!=(table[haut].length-1)*20) {
+			if(table[haut][larg+1]==0) {
+				smile.style.left=x+20+'px';
+				larg++;
+				document.getElementById('largeur').innerHTML = larg;
+				document.getElementById("pas").innerHTML++;
+			} else if(table[haut][larg+1]==3) {
+				win();
+			}
+		}
+	}
+
+	if(key==40) { // Si on appuie sur fleche bas
+		if(y!=(table.length-1)*20) {
+			if(table[haut+1][larg]==0) {
+				smile.style.top=y+20+'px';
+				haut++;
+				document.getElementById('haut').innerHTML = haut;
+				document.getElementById("pas").innerHTML++;
+			} else if(table[haut+1][larg]==3) {
+				win();
+			}
+		}
+	}
+}
+
+/**
+ * Affiche la template
+ *
+ * @param template un tableau à 2 dimensions
+ * */
+function displayTemplate(template) {
+	for(var i=0;i<template.length;i++){
+		for(var j=0;j<template[i].length;j++){
+
+			// On construit la carte en fonction des valeurs des cases
+			switch (template[i][j]) {
+				case 1:
+					var conteneur = document.getElementById("conteneur");
+					var monImg = document.createElement('img');
+					monImg.setAttribute('class','mur');
+					monImg.src="./assets/img/mur.png";
+					monImg.style.height='20px';
+					monImg.style.width='20px';
+					monImg.style.position='absolute';
+					monImg.style.top=i*20+'px';
+					monImg.style.left=j*20+'px';
+					conteneur.appendChild(monImg);
+				break;
+
+/*				case 0:
+					var conteneur = document.getElementById("conteneur");
+					var monImg = document.createElement('img');
+					monImg.src="./assets/img/blanc.png";
+					monImg.style.height='20px';
+					monImg.style.width='20px';
+					monImg.style.position='absolute';
+					monImg.style.top=i*20+'px';
+					monImg.style.left=j*20+'px';
+					conteneur.appendChild(monImg);
+				break;*/
+
+				// Position de départ du joueur
+				case 2:
+					var conteneur = document.getElementById("conteneur");
+					var monImg = document.getElementById("pic2");
+					monImg.src="./assets/img/personnage.png";
+					monImg.style.height='20px';
+					monImg.style.width='20px';
+					monImg.style.position='absolute';
+					monImg.style.top=i*20+'px';
+					monImg.style.left=j*20+'px';
+					haut=i;
+					larg=j;
+					document.getElementById('haut').innerHTML = haut;
+					document.getElementById('largeur').innerHTML = larg;
+					conteneur.appendChild(monImg);
+				break;
+
+				// Position de la fleche
+				case 3:
+					var conteneur = document.getElementById("conteneur");
+					var monImg = document.createElement('img');
+					monImg.style.visibility = "visible";
+					monImg.src="./assets/img/fleche.png";
+					monImg.style.height='20px';
+					monImg.style.width='20px';
+					monImg.style.position='absolute';
+					monImg.style.top=i*20+'px';
+					monImg.style.left=j*20+'px';
+					conteneur.appendChild(monImg);
+				break;
+			}
+		}
+	}
+}
+
+function win() {
+	reset(table);
+
+	table=[
+		[2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1],
+		[0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1],
+		[0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1],
+		[0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1],
+		[0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1],
+		[0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1],
+		[0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	];
+
+
+/*	for(var i=0;i<=19;i++){
+		for(var j=0;j<=19;j++){
+			if(table[i][j]==1){
+				afficherMur(i,j);
+			}
+		}
+	}*/
+
+	displayTemplate(table);
+
+	// On affiche le bouton niveau suivant
+	var bouton=document.getElementById("niveau");
+	bouton.style.visibility="visible";
+}
+
 function niveau1() {
-	//Création du terrain 1
+/*	//Création du terrain 1
+	//Placement du joueur
+	document.getElementById("pic2").style.top='0px';
+	document.getElementById("pic2").style.left='0px';
 	//Placement de la fleche de sortie
 	document.getElementById("fleche").style.top=19*20+'px';
 	document.getElementById("fleche").style.left=10*20+'px';
@@ -53,8 +266,69 @@ function niveau1() {
 	for(var i=0;i<=19;i++){
 		table[i][12]=1;
 	}
+	table[19][10]=2;
+*/
 
+	table = [
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0],
+		[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0],
+		[0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	];
+
+	//displayTemplate(table);
 }
+
+function niveau2() {
+/*	//Création du terrain 1
+	//Placement du joueur
+	document.getElementById("pic2").style.top='0px';
+	document.getElementById("pic2").style.left='0px';
+	//Placement de la fleche de sortie
+	document.getElementById("fleche").style.top=19*20+'px';
+	document.getElementById("fleche").style.left=10*20+'px';
+	//Initialisation du terrain (pose de mur)
+*/
+	table = [
+		[2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0],
+		[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 1, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	];
+}
+
 
 function retry() {
 	document.getElementById("pic2").style.top='0px';
@@ -63,20 +337,40 @@ function retry() {
 	larg=0;
 	haut=0;
 }
-/*
-function displayMine() {
+
+function afficherMur(i,j){
 	var conteneur = document.getElementById("conteneur");
 	var monImg = document.createElement('img');
-	monImg.src="./croix.png";
-	monImg.style.height='18px';
-	monImg.style.width='18px';
+	monImg.setAttribute('class','mur');
+	monImg.src="./assets/img/mur.png";
+	monImg.style.height='20px';
+	monImg.style.width='20px';
 	monImg.style.position='absolute';
-	monImg.style.top=haut*20+'px';
-	monImg.style.left=larg*20+'px';
+	monImg.style.top=i*20+'px';
+	monImg.style.left=j*20+'px';
 	conteneur.appendChild(monImg);
 }
-*/
-/*
+
+function enleverMur(i,j){
+	var conteneur = document.getElementById("conteneur");
+	var monImg = document.createElement('img');
+	monImg.src="./assets/img/blanc.png";
+	monImg.style.height='20px';
+	monImg.style.width='20px';
+	monImg.style.position='absolute';
+	monImg.style.top=i*20+'px';
+	monImg.style.left=j*20+'px';
+	conteneur.appendChild(monImg);
+}
+
+function niveauSuivant() {
+	niveau+=1;
+	document.getElementById('vueNiveau').innerHTML++;
+	console.log(niveau);
+	reset(table);
+	start();
+}
+
 function cheat() {
 	var h=haut;
 	var l=larg;
@@ -96,8 +390,7 @@ function cheat() {
 	haut=h;
 	larg=l;
 }
-*/
-/*
+
 function verifMine() {
 	var nb=0;
 	if(haut!=0) {
@@ -122,99 +415,5 @@ function verifMine() {
 	}
 	document.getElementById("mine").innerHTML=nb;
 }
-*/
 
-// gestion des mouvements joueurs
-function move(event) {
-	var key=event.keyCode;
 
-	var smile=document.getElementById("pic2");
-	var x=smile.offsetLeft;
-	var y=smile.offsetTop;
-
-	if(key==65) { // 65 représente la touche "a"
-		cheat();
-	}
-
-	if(key==37) { // 37 représente la touche fleche gauche
-		if(x!=0) {
-			if(table[haut][larg-1]!=1) {
-				smile.style.left=x-20+'px';
-				larg--;
-				document.getElementById("pas").innerHTML++;
-				//verifMine();
-				if(table[haut][larg]==2) {
-					alert('win');
-					start();
-				}
-			}/* else {
-				larg--;
-				document.getElementById("pas").innerHTML-=10;
-				displayMine();
-				alert('die');
-				retry();
-			}*/
-		}
-	}
-
-	if(key==38) { // 38 représente la touche fleche haut
-		if(y!=0) {
-			if(table[haut-1][larg]!=1) {
-				smile.style.top=y-20+'px';
-				haut--;
-				document.getElementById("pas").innerHTML++;
-				//verifMine();
-				if(table[haut][larg]==2) {
-					alert('win');
-					start();
-				}
-			} /*else {
-				haut--;
-				document.getElementById("pas").innerHTML-=10;
-				displayMine();
-				alert('die');
-				retry();
-			}*/
-		}
-	}
-	if(key==39) { // 39 représente la touche fleche droite
-		if(x!=380) {
-			if(table[haut][larg+1]!=1) {
-				smile.style.left=x+20+'px';
-				larg++;
-				document.getElementById("pas").innerHTML++;
-				//verifMine();
-				if(table[haut][larg]==2) {
-					alert('win');
-					start();
-				}
-			}/* else {
-				larg++;
-				document.getElementById("pas").innerHTML-=10;
-				displayMine();
-				alert('die');
-				retry();
-			}*/
-		}
-	}
-	if(key==40) { // Si on appuie sur fleche bas
-		if(y!=380) {
-			if(table[haut+1][larg]!=1) {
-				smile.style.top=y+20+'px';
-				haut++;
-				document.getElementById("pas").innerHTML++;
-				//verifMine();
-				if(table[haut][larg]==2) {
-					alert('win');
-					start();
-				}
-			}/* else {
-				haut++;
-				document.getElementById("pas").innerHTML-=10;
-				displayMine();
-				alert('die');
-				retry();
-			}*/
-		}
-	}
-}
